13b5de88e83e44eb11c52c88bb8cdd136f8dac96
Harun Davood <Harun.Davood@exeloncorp.com>
2020-11-05 18:17:19 -0500

    updates version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 // Modules
 module "web_tier_6" {
   source  = "app.terraform.io/eudigital/web-tier-6/azurerm"
-  version = "1.0.3"
+  version = "1.0.4"
 
   project_name = "POC"
   project_short_name = "poc"
